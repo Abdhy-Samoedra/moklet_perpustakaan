6b87a3f0dfcac9255913f5c8b7765e85e003965c
Abdhy-Samoedra <54672937+Abdhy-Samoedra@users.noreply.github.com>
2020-03-24 14:59:35 +0700

    Create regist.php

---
+++ regist.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>masuk/daftar</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <form class="box" action="login.php" method="post">
+      <h1>Masuk / Daftar</h1>
+      <input type="text" name="username" placeholder="Username">
+      <input type="password" name="password" placeholder="Password">
+      <input type="submit" name="login" value ="login">
+      <a href="daftar.php">Daftar</a>
+    </form>
+  </body>
+</html>
