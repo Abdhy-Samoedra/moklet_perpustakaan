65da52ade689eca5d70f88ecd2fe2215715f3beb
Abdhy-Samoedra <54672937+Abdhy-Samoedra@users.noreply.github.com>
2020-03-24 14:58:50 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+include 'aset/header.php';
+?>
+<div class="container">
+  <div class="row mt-4">
+    <div class="col-md">
+      <h2 align="center"><i class="fas fa-book"></i> Perpustakaan SMK Telkom Malang</h2>
+      <hr class="bg-light">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-4">
+      <div class="card bg-danger" style="width: 18rem;">
+  <div class="card-body text-white">
+    <h5 class="card-title">masuk sebagai</h5>
+    <p class="card-text" style="font-size:60px">Anggota</p>
+    <a href="http://localhost/moklet_perpustakaan/regist.php" class="text-white">masuk <i class="fas fa-angle-double-right"></i></a>
+  </div>
+</div>
+    </div>
+    <div class="col-md-4">
+      <div class="card bg-primary" style="width: 18rem;">
+  <div class="card-body text-white">
+    <h5 class="card-title">masuk sebagai</h5>
+    <p class="card-text" style="font-size:60px">Petugas</p>
+    <a href="" class="text-white">masuk <i class="fas fa-angle-double-right"></i></a>
+  </div>
+    </div>
+  </div>
+<?php
+include 'aset/footer.php';
+ ?>
